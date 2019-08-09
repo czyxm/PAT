fc93bfa212e681af1b469b086e12a88f4befba14
czyxm <cmzyx@zju.edu.cn>
2019-08-09 17:19:27 +0800

    Advanced level 1057

--- AdvancedLevel/1057Stack.cpp
+++ AdvancedLevel/1057Stack.cpp
@@ -3,43 +3,35 @@
 #include<queue>
 #include<string>
 #include<algorithm>
+#define MAXSIZE 100001
+#define SQRSIZE 320
 using namespace std;
 
-int partition(int left, int right, vector<int> & array)
-{
-    int pivot = right;
-    right--;
-    while (left <= right)
-    {
-        while (left < pivot && array[left] <= array[pivot]) {left++;}
-        while (right >= 0 && array[right] > array[pivot]) {right--;}
-        if (left >= right)
-        {
-            break;
-        }
-        swap(array[left], array[right]);
-    }
-    swap(array[left], array[pivot]);
-    return left;
-}
+int block[SQRSIZE] = {0};
+int table[MAXSIZE] = {0};
 
 int main()
 {
     vector<int> stack, _stack;
     string command;
-    int N, key, k, index, left, right;
+    int i, j, N, key, k, index, sum, left, right;
     cin >> N;
-    for (int i = 1; i <= N; i++)
+    for (i = 1; i <= N; i++)
     {
         cin >> command;
         if (command == "Push")
         {
             cin >> key;
+            table[key]++;
+            block[key / SQRSIZE]++;
             stack.push_back(key);
         }
         else if (command == "Pop" && stack.size() > 0)
         {
-            cout << stack.back() << endl;
+            key = stack.back();
+            cout << key << endl;
+            table[key]--;
+            block[key / SQRSIZE]--;
             stack.pop_back();
         }
         else if (command == "PeekMedian" && stack.size() > 0)
@@ -47,24 +39,17 @@ int main()
             k = stack.size();
             if ((k & 1) == 1){k++;}
             k >>= 1;
-            k--;
-            index = -1;
-            left = 0;
-            right = stack.size() - 1;
-            _stack = stack;
-            while (index != k)
+            sum = 0;
+            for (j = 0; j < SQRSIZE && sum + block[j] < k; j++)
             {
-                index = partition(left, right, _stack);
-                if (index < k)
-                {
-                    left = index + 1;
-                }
-                else if (index > k)
-                {
-                    right = index - 1;
-                }
+                sum += block[j];
             }
-            cout << _stack[k] << endl;
+            index = SQRSIZE * j;
+            while (sum < k)
+            {
+                sum += table[index++];
+            }
+            cout << index - 1 << endl;
         }
         else
         {
