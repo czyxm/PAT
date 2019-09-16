c85076329b7dc9411e99a8378d6981b34e57f381
czyxm <cmzyx@zju.edu.cn>
2019-09-16 15:26:01 +0800

    Advanced level 1026

--- AdvancedLevel/1026TableTennis.cpp
+++ AdvancedLevel/1026TableTennis.cpp
@@ -1,154 +1,168 @@
-#include<iostream>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-//Structure node for player
-typedef struct PlayerNode
+
+struct Player
 {
-    int arriveTime;
-    int serveTime;
-    int waitTime;
-    int playTime;
-    int isVIP;
+    int arriveTime, serveTime, playTime, isVIP;
     bool known;
-}PNode;
-//Structure node for table
-typedef struct TableNode
+    bool operator< (const Player & p)const
+    {
+        return arriveTime < p.arriveTime;
+    }
+};
+struct Table
 {
-    int serveNumber;
-    int spareTime;
-    int isVIP;
-}TNode;
-//Compare function for sorting players by arrive time
-bool compare(const PNode & p1, const PNode & p2)
+    int serveNumber, spareTime, isVIP;
+    Table():serveNumber(0), spareTime(8 * 3600), isVIP(0){}
+};
+
+vector<Player> player;
+vector<Table> table;
+vector<int> res;
+
+int toSeconds(const string & str)
 {
-    return p1.arriveTime < p2.arriveTime;
+    return stoi(str.substr(0, 2)) * 3600 + stoi(str.substr(3, 2)) * 60 + stoi(str.substr(6, 2));
 }
 
 int main()
 {
-    int N, K, M, tableIndex, vipTableIndex, playerIndex;
-    string timeStr;
-    vector<PNode> player;
-    vector<TNode> table;
-    vector<PNode> servedPlayer;
+    int N, K, M, tIndex, pIndex, index = 0, vipTableIndex;
+    string str;
     cin >> N;
     player.resize(N);
     for (int i = 0; i < N; i++)
     {
-        cin >> timeStr >> player[i].playTime >> player[i].isVIP;
-        //Play time must be at most 2 hours
+        cin >> str >> player[i].playTime >> player[i].isVIP;
+        player[i].arriveTime = toSeconds(str);
         if (player[i].playTime > 120)
         {
             player[i].playTime = 120;
         }
+        player[i].playTime *= 60;
         player[i].known = false;
-        player[i].playTime *= 60;  //Transfer to seconds
-        player[i].arriveTime = stoi(timeStr.substr(0, 2)) * 3600 + stoi(timeStr.substr(3, 2)) * 60 + stoi(timeStr.substr(6, 2));
     }
-    sort(player.begin(), player.end(), compare);
+    sort(player.begin(), player.end());
     cin >> K >> M;
-    table.resize(K);
-    for (int i = 0; i < K; i++)
-    {
-        table[i].serveNumber = table[i].spareTime = table[i].isVIP = 0;
-    }
+    table.resize(K + 1);
     for (int i = 1; i <= M; i++)
     {
-        cin >> tableIndex;
-        table[tableIndex - 1].isVIP = 1;
+        cin >> tIndex;
+        table[tIndex].isVIP = 1;
     }
-
-    for (int i = 0; i < player.size(); i++)
+    while (index < player.size())
     {
-        if (player[i].known) 
+        tIndex = vipTableIndex = 0;
+        pIndex = index;
+        bool found = false;
+        //The next player comes when there are some spare tables
+        for (int i = 1; i <= K; i++)
         {
-            continue;
-        }
-        //Find the index of the table that finish its current service earliset 
-        tableIndex = 0; 
-        for (int j = 1; j < K; j++)
-        {
-            if (table[j].spareTime < table[tableIndex].spareTime)
+            if (table[i].spareTime <= player[index].arriveTime)
             {
-                tableIndex = j;
-            }
-        }
-        //If the next player comes later than the table's spare time
-        if (player[i].arriveTime >= table[tableIndex].spareTime)
-        {
-            if (player[i].arriveTime >= 21 * 3600)
-            {
-                break;
-            }
-            if (player[i].isVIP == 1)
-            {
-                vipTableIndex = -1;
-                for (int j = 0; j < K; j++)
+                if (!found)
                 {
-                    if (table[j].isVIP == 1 && player[i].arriveTime >= table[j].spareTime)
-                    {
-                        if (vipTableIndex == -1 || table[j].spareTime < table[vipTableIndex].spareTime)
-                            vipTableIndex = j;
-                    }
+                    tIndex = i;
+                    found = true;
                 }
-                if (vipTableIndex != -1)
+                if (table[i].isVIP == 1)
                 {
-                    tableIndex = vipTableIndex;
+                    vipTableIndex = i;
+                    break;
                 }
             }
-            player[i].waitTime = 0;
-            table[tableIndex].spareTime = player[i].arriveTime + player[i].playTime;
-            servedPlayer.push_back(player[i]);
-            table[tableIndex].serveNumber++;
         }
-        //If the next player comes earlier than the table's spare time
+        //The next player comes when there is no spare table
+        if (tIndex == 0)
+        {
+            tIndex = 1;
+            for (int i = 2; i <= K; i++)
+            {
+                if (table[i].spareTime < table[tIndex].spareTime)
+                {
+                    tIndex = i;
+                }
+            }
+            for (int i = 1; i <= K; i++)
+            {
+                if (table[i].spareTime == table[tIndex].spareTime && table[i].isVIP == 1)
+                {
+                    vipTableIndex = i;
+                    break;
+                }
+            }
+        }
+        //Find next vip player in the queue
+        if (vipTableIndex > 0)
+        {
+            for (int i = index; i < N && player[i].arriveTime <= table[tIndex].spareTime; i++)
+            {
+                if (!player[i].known && player[i].isVIP == 1)
+                {
+                    pIndex = i;
+                    break;
+                }
+            }
+        }
+        if (player[pIndex].isVIP == 1 && vipTableIndex > 0)
+        {
+            tIndex = vipTableIndex;
+        }
+        if (player[pIndex].arriveTime <= table[tIndex].spareTime)
+        {
+            player[pIndex].serveTime = table[tIndex].spareTime;
+        }
         else
         {
-            if (table[tableIndex].spareTime >= 21 * 3600)
-            {
-                break;
-            }
-            playerIndex = i;
-            if (table[tableIndex].isVIP == 1)
-            {
-                for (int j = i; j < player.size() && player[j].arriveTime <= table[tableIndex].spareTime; j++)
-                {
-                    if (player[j].isVIP == 1)
-                    {
-                        playerIndex = j;
-                        break;
-                    }
-                }
-            }
-            player[playerIndex].waitTime = table[tableIndex].spareTime - player[playerIndex].arriveTime;
-            servedPlayer.push_back(player[playerIndex]); 
-            table[tableIndex].spareTime += player[playerIndex].playTime;
-            table[tableIndex].serveNumber++;
-            if (playerIndex != i)
-            {
-                player[playerIndex].known = true;
-                i--;
-            }
+            player[pIndex].serveTime = player[pIndex].arriveTime;
+        }
+        if (player[pIndex].serveTime < 21 * 3600)
+        {
+            res.push_back(pIndex);
+        }
+        else
+        {
+            break;
+        }
+        player[pIndex].known = true;
+        table[tIndex].serveNumber++;
+        table[tIndex].spareTime = player[pIndex].serveTime + player[pIndex].playTime;
+        while (player[index].known)
+        {
+            index++;
         }
     }
-    for (int i = 0; i < servedPlayer.size(); i++)
+    for (int i : res)
     {
-        servedPlayer[i].serveTime = servedPlayer[i].arriveTime + servedPlayer[i].waitTime;
-        cout << setfill('0') << setw(2) << servedPlayer[i].arriveTime / 3600 << ":" << setfill('0') << setw(2) << servedPlayer[i].arriveTime / 60 % 60 << ":" << setfill('0') << setw(2) << servedPlayer[i].arriveTime % 60 << " ";
-        cout << setfill('0') << setw(2) << servedPlayer[i].serveTime / 3600 << ":" << setfill('0') << setw(2) << servedPlayer[i].serveTime / 60 % 60 << ":" << setfill('0') << setw(2) << servedPlayer[i].serveTime % 60 << " ";
-        cout << (servedPlayer[i].waitTime + 30) / 60 << endl;
+        cout << setw(2) << setfill('0') << player[i].arriveTime / 3600 << ":" << setw(2) << setfill('0') << player[i].arriveTime / 60 % 60 << ":" << setw(2) << setfill('0') << player[i].arriveTime % 60 << " ";
+        cout << setw(2) << setfill('0') << player[i].serveTime / 3600 << ":" << setw(2) << setfill('0') << player[i].serveTime / 60 % 60 << ":" << setw(2) << setfill('0') << player[i].serveTime % 60 << " ";
+        cout << (player[i].serveTime - player[i].arriveTime + 30) / 60 << endl;
     }
-    for (int i = 0; i < table.size(); i++)
+    for (int i = 1; i <= K; i++)
     {
-        cout << table[i].serveNumber;
-        if (i < table.size() - 1)
+        if (i != 1)
         {
             cout << " ";
         }
+        cout << table[i].serveNumber;
     }
-    cout << endl;
     return 0;
 }
+/*Some test data
+2
+10:00:00 30 1
+12:00:00 30 1
+5 1
+3
+
+2
+18:00:00 180 1
+20:00:00 60 1
+1 1
+1
+
+1
+21:00:00 80 1
+1 1
+1
+*/
